32e35117f22df2cac2d589223bc905ce85fdbe7e
Bane <galovicbranislav@gmail.com>
2025-06-04 15:20:56 +0200

    Finished with BMP280 code. Need to test it

--- components/BMP280_Component/BMP280_Component.c
+++ components/BMP280_Component/BMP280_Component.c
@@ -11,7 +11,9 @@ void bmp280_init(void)
     bmp280.address = bmp280_address;
     bmp280.ctrl_meas = bmp280_ctrl_meas;
     bmp280.config = bmp280_config;
+    bmp280.calib = bmp280_calib;
     bmp280.setup_commands = bmp280_setup;
+    bmp280.temp_reg_address = bmp280_press_msb;
     bmp280.SCL = GPIO_NUM_37;
     bmp280.SDA = GPIO_NUM_38;
 
@@ -56,4 +58,69 @@ void bmp280_prepare_transmit_setup(void)
 
     bmp280.transmit_setup[3].write_buffer = &bmp280.config_reg_val;
     bmp280.transmit_setup[3].buffer_size = ONE_BYTE;
+}
+
+/*
+Get calibration parameters for the compensation formula
+*/
+void bmp280_compensation_parameters(void)
+{
+    i2c_send_data(bmp280.bmp280_dev_handle, &bmp280.calib, ONE_BYTE); // check if this generates STOP condition
+    i2c_read_data(bmp280.bmp280_dev_handle, &bmp280.calib_data, 26);
+
+    bmp280.dig_T1 = (uint16_t)(bmp280.calib_data[1] << 8 | bmp280.calib_data[0]);
+    bmp280.dig_T2 = (int16_t)(bmp280.calib_data[3] << 8 | bmp280.calib_data[2]);
+    bmp280.dig_T3 = (int16_t)(bmp280.calib_data[5] << 8 | bmp280.calib_data[4]);
+    bmp280.dig_P1 = (uint16_t)(bmp280.calib_data[7] << 8 | bmp280.calib_data[6]);
+    bmp280.dig_P2 = (int16_t)(bmp280.calib_data[9] << 8 | bmp280.calib_data[8]);
+    bmp280.dig_P3 = (int16_t)(bmp280.calib_data[11] << 8 | bmp280.calib_data[10]);
+    bmp280.dig_P4 = (int16_t)(bmp280.calib_data[13] << 8 | bmp280.calib_data[12]);
+    bmp280.dig_P5 = (int16_t)(bmp280.calib_data[15] << 8 | bmp280.calib_data[14]);
+    bmp280.dig_P6 = (int16_t)(bmp280.calib_data[17] << 8 | bmp280.calib_data[16]);
+    bmp280.dig_P7 = (int16_t)(bmp280.calib_data[19] << 8 | bmp280.calib_data[18]);
+    bmp280.dig_P8 = (int16_t)(bmp280.calib_data[21] << 8 | bmp280.calib_data[20]);
+    bmp280.dig_P9 = (int16_t)(bmp280.calib_data[23] << 8 | bmp280.calib_data[22]);
+}
+
+void bmp280_read_raw_data(void)
+{
+    i2c_send_data(bmp280.bmp280_dev_handle, &bmp280.temp_reg_address, ONE_BYTE);
+    i2c_read_data(bmp280.bmp280_dev_handle, &bmp280.raw_data, 6);
+
+    bmp280.adc_P = ((int32_t)bmp280.raw_data[0] << 12) | ((int32_t)bmp280.raw_data[1] << 4) | (bmp280.raw_data[2] >> 4);
+    bmp280.adc_T = ((int32_t)bmp280.raw_data[3] << 12) | ((int32_t)bmp280.raw_data[4] << 4) | (bmp280.raw_data[5] >> 4);
+}
+
+int32_t bmp280_compensate_T_int32(int32_t adc_T)
+{
+    int32_t var1, var2, T;
+    var1 = ((((adc_T >> 3) - ((int32_t)bmp280.dig_T1 << 1))) * ((int32_t)bmp280.dig_T2)) >> 11;
+    var2 = (((((adc_T >> 4) - ((int32_t)bmp280.dig_T1)) * ((adc_T >> 4) - ((int32_t)bmp280.dig_T1))) >> 12) *
+            ((int32_t)bmp280.dig_T3)) >> 14;
+    bmp280.t_fine = var1 + var2;
+    T = (bmp280.t_fine * 5 + 128) >> 8;  // Temperature in 0.01 °C
+
+    return T;
+}
+
+uint32_t bmp280_compensate_P_int64(int32_t adc_P)
+{
+    int64_t var1, var2, p;
+    var1 = ((int64_t)bmp280.t_fine) - 128000;
+    var2 = var1 * var1 * (int64_t)bmp280.dig_P6;
+    var2 = var2 + ((var1 * (int64_t)bmp280.dig_P5) << 17);
+    var2 = var2 + (((int64_t)bmp280.dig_P4) << 35);
+    var1 = ((var1 * var1 * (int64_t)bmp280.dig_P3) >> 8) + ((var1 * (int64_t)bmp280.dig_P2) << 12);
+    var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)bmp280.dig_P1) >> 33;
+
+    if (var1 == 0) return 0;  // avoid divide by zero
+
+    p = 1048576 - adc_P;
+    p = (((p << 31) - var2) * 3125) / var1;
+    var1 = ((int64_t)bmp280.dig_P9 * (p >> 13) * (p >> 13)) >> 25;
+    var2 = ((int64_t)bmp280.dig_P8 * p) >> 19;
+
+    p = ((p + var1 + var2) >> 8) + (((int64_t)bmp280.dig_P7) << 4);
+
+    return (uint32_t)p;  // pressure in Pa (Pascal), Q24.8 format
 }
--- components/BMP280_Component/include/BMP280_Component.h
+++ components/BMP280_Component/include/BMP280_Component.h
@@ -13,6 +13,7 @@ Addresses
 #define bmp280_id           0xD0
 #define bmp280_address      0x76 // If connected to VDDIO then slave address ix 0x77. With 0x76 it's connected to ground. 5.2
 #define bmp280_setup        0x06 // Disable temperature, enable pressure and choose force mode
+#define bmp280_calib        0x88
 
 /*
 BMP280 struct
@@ -24,11 +25,34 @@ typedef struct
     uint8_t address;
     uint8_t ctrl_meas;
     uint8_t config;
+    uint8_t calib;
     uint8_t setup_commands;
     gpio_num_t SCL;
     gpio_num_t SDA;
     i2c_master_transmit_multi_buffer_info_t transmit_setup[4];
+    uint8_t calib_data[26];
     uint8_t config_reg_val;
+    uint8_t raw_data[6];
+    uint8_t temp_reg_address;
+    int32_t adc_P;
+    int32_t adc_T;
+    int32_t t_fine;
+
+    /*
+    Calibration parameters
+    */
+    uint16_t dig_T1;
+    int16_t dig_T2;
+    int16_t dig_T3;
+    uint16_t dig_P1;
+    int16_t dig_P2;
+    int16_t dig_P3;
+    int16_t dig_P4;
+    int16_t dig_P5;
+    int16_t dig_P6;
+    int16_t dig_P7;
+    int16_t dig_P8;
+    int16_t dig_P9;
 
 } bmp280_t;
 
@@ -39,3 +63,7 @@ void bmp280_init(void);
 void bmp280_prepare_transmit_setup(void);
 void bmp280_config_regs(void);
 void bmp280_start_measure(void);
+void bmp280_compensation_parameters(void);
+void bmp280_read_raw_data(void);
+int32_t bmp280_compensate_T_int32(int32_t adc_T);
+uint32_t bmp280_compensate_P_int64(int32_t adc_P);
